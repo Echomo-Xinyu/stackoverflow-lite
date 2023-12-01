e721c1363f96c67bf39af5e513dfc4e8884085d9
Echomo Xinyu <seanxinyu2@gmail.com>
2023-11-30 22:41:21 -0800

    lay down general framework

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './Root';
+import Index from './routes/Index';
+import { fetchPost } from './api';
+import Post from './routes/Post';
+import User from './routes/User';
+import Admin from './routes/Admin';
+import AdminHome from './routes/AdminHome';
+import AdminHighlight from './routes/AdminHighlight';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Index />,
+        loader() {
+          // TODO
+          return null;
+        },
+      },
+      {
+        path: "/post/:postId",
+        element: <Post />,
+        loader({ params }) {
+          // TODO
+          return null;
+        },
+      },
+      {
+        path: "user/:userId",
+        element: <User />,
+        loader({ params }) {
+          // TODO
+          return null;
+        },
+      },
+      {
+        path: "admin",
+        element: <Admin />,
+        children: [
+          {
+            path: "",
+            element: <AdminHome />,
+            loader() {
+              // TODO
+          return null;
+            },
+          },
+          {
+            path: "highlight",
+            element: <AdminHighlight />,
+            loader() {
+              // TODO
+              return null;
+            }
+          }
+        ]
+        
+        
+      },
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
