92c4ad53cf0013b0665838ca1f3a8e4df583dc26
Echomo Xinyu <seanxinyu2@gmail.com>
2023-11-30 22:42:07 -0800

    add navigation bar

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,6 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Navigation from "./routes/Navigation";
+
 export default function Root() {
   return (
-    <>
-    </>
+    <div className="container">
+      <Navigation />
+      <Outlet />
+    </div>
   )
 }
