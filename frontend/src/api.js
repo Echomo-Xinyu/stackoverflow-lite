885dec68a920a6f05111747eb32889e7db851406
Echomo Xinyu <seanxinyu2@gmail.com>
2023-12-01 00:07:39 -0800

    implement corresponding api calls

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,27 +1,150 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-// anyone can make a post / post a comment using self-declared name
-// the name can be repeated and will be credited to same person
+// anyone can make a post / write an answer / post a comment using self-declared name
+//   name is unique to one person
 // anyone can view the comment / post by a particular person
-// only admin can edit or delete a post / comment
-
-// query all posts
-// add post
-// (admin) update post
-// (admin) delete post
-// query answers of specific post
-// add answer to specific post
-// (admin) update answer
-// (admin) delete answer
-// add comment to specific answer
-// (admin) update comment information
-// (admin) delete comment
-// query all users
-// query user's post, answer, and comment
-// add user (self-declared name, auto increment id)
-
+// only admin can edit or delete a post / answer / comment
 
 // post: id, title, body, isStarred, userId, timestamp
 // answer: id, body, userId, postId, timestamp
 // comment: id, body, answerId, postId, userId, timestamp
 // user: id, name
+
+// query all posts
+export function fetchPost() {
+  return fetch(`${baseUrl}/posts?_expand=user`)
+    .then((response) => response.json());
+}
+
+// add post
+export function addPost(data) {
+  return fetch(`${baseUrl}/posts`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+// (admin) update post
+export function updatePost(postId, data) {
+  return fetch(`${baseUrl}/posts/${postId}`, {
+    method: "PATCH",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+// (admin) delete post
+export function deletePost(postId) {
+  return fetch(`${baseUrl}/posts/${postId}`, {
+    method: "DELETE",
+  });
+}
+
+// /answers?_expand=user&_expand=post to expand multiple fields
+// query a specific post
+export function fetchAnswers() {
+  return fetch(`${baseUrl}/answers?_expand=user&_expand=post`)
+    .then((response) => response.json());
+}
+
+export function fetchComments() {
+  return fetch(`${baseUrl}/comments`)
+    .then((response) => response.json());
+}
+
+// add answer to specific post
+export function addAnswers(data) {
+  return fetch(`${baseUrl}/answers`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+// (admin) update answer
+export function updateAnswers(answerId, data) {
+  return fetch(`${baseUrl}/answers/${answerId}`, {
+    method: "PATCH",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+// (admin) delete answer
+export function deleteAnswer(answerId) {
+  return fetch(`${baseUrl}/answers/${answerId}`, {
+    method: "DELETE",
+  });
+}
+
+// add comment to specific answer
+export function addComment(data) {
+  return fetch(`${baseUrl}/comments`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+// (admin) update comment information
+export function updateComment(commendId, data) {
+  return fetch(`${baseUrl}/comments/${commendId}`, {
+    method: "PATCH",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+// (admin) delete comment
+export function deleteComment(commendId) {
+  return fetch(`${baseUrl}/comments/${commendId}`, {
+    method: "DELETE",
+  });
+}
+
+// query all users
+export function fetchUsers() {
+  return fetch(`${baseUrl}/users`)
+    .then((response) => response.json());
+}
+
+// query user's post, answer, and comment
+// achieved by combing fetchPosts(), fetchAnswers(), fetchComments()
+
+// add user (self-declared name, auto increment id)
+export function addUser(data) {
+  return fetch(`${baseUrl}/users`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
