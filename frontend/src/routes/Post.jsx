59b3663c0f4fd6b3c20afed62d75584031753f38
Echomo Xinyu <seanxinyu2@gmail.com>
2023-12-03 22:00:59 -0800

    add update option during admin mode

--- frontend/src/routes/Post.jsx
+++ frontend/src/routes/Post.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, Link } from "react-router-dom";
+import { useAdmin } from "./AdminContext";
 import Input from "./Input";
 import {
   addAnswer,
@@ -7,12 +8,16 @@ import {
   addUser,
   fetchAnswers,
   fetchComments,
+  fetchPostById,
   fetchUsers,
+  updateAnswer,
+  updateComment,
+  updatePost,
 } from "../api";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Post() {
-  const postData = useLoaderData();
+  const [post, setPost] = useState(useLoaderData());
   const [answers, setAnswers] = useState([]);
   const [comments, setComments] = useState([]);
   const [isCommentFormExpaned, setIsCommentFormExpanded] = useState(false);
@@ -21,17 +26,23 @@ export default function Post() {
   const [newCommentAuthor, setNewCommentAuthor] = useState("");
   const [newAnswerBody, setNewAnswerBody] = useState("");
   const [newAnswerAuthor, setNewAnswerAuthor] = useState("");
+  const { isAdmin, toggleAdmin } = useAdmin();
+  const [editContentId, setEditContentId] = useState("");
+  const [editContentBody, setEditContentBody] = useState("");
 
   const fetchData = async () => {
+    const newPostData = await fetchPostById(post.id);
+    setPost(newPostData);
+
     const answersData = await fetchAnswers();
     const filteredAnswers = answersData.filter(
-      (answer) => answer.postId === postData.id
+      (answer) => answer.postId === post.id
     );
     setAnswers(filteredAnswers);
 
     const commentsData = await fetchComments();
     const filteredComments = commentsData.filter(
-      (comment) => comment.postId === postData.id
+      (comment) => comment.postId === post.id
     );
     setComments(filteredComments);
   };
@@ -42,130 +53,294 @@ export default function Post() {
 
   return (
     <div className="container">
-      <ContentBody type="Post:" content={postData} />
-      {answers
-      .sort((answerA, answerB) => {
-        return answerB.timestamp - answerA.timestamp;
-      })
-      .map((answer) => {
-        return (
-          <div key={"answers" + answer.id} className="container">
-            <br />
-            <ContentBody type="Answer: " content={answer} />
-            {comments.length > 0 ? <p>Comments sorted from latest to oldest</p> : <></>}
-            {comments
-              .filter((comment) => {
-                return comment.answerId === answer.id;
-              })
-              .sort((commentA, commentB) => {
-                return commentB.timestamp - commentA.timestamp;
-              })
-              .map((comment) => {
-                return (
-                  <ContentBody
-                    key={"comment" + comment.id}
-                    type="Comment: "
-                    content={comment}
-                  />
-                );
-              })}
-            <button
-              onClick={(event) => {
-                if (!isCommentFormExpaned) {
-                  setIsCommentFormExpanded(true);
-                  setNewCommentAnswerId(answer.id);
-                } else {
-                  // if already expanded, then one more click will make it collapse
-                  if (answer.id === newCommentAnswerId) {
-                    setIsCommentFormExpanded(false);
-                    setNewCommentAnswerId("");
-                  } else {
-                    setNewCommentAnswerId(answer.id);
-                  }
-                }
+      <ContentBody type="Post:" content={post} />
+      {isAdmin ? (
+        <>
+          <button
+            onClick={() => {
+              setEditContentId(post.id);
+            }}
+          >
+            Edit
+          </button>
+          {post.id === editContentId ? (
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                const handleUpdatePost = async () => {
+                  const newTimestamp = Date.now();
+
+                  await updatePost(post.id, {
+                    id: post.id,
+                    title: "",
+                    body: editContentBody,
+                    isStarred: "f",
+                    userId: post.userId,
+                    timestamp: newTimestamp,
+                  });
+                  setEditContentBody("");
+                  setEditContentId("");
+                  fetchData();
+                };
+                handleUpdatePost();
               }}
             >
-              Add a comment to this answer
-            </button>
-            {isCommentFormExpaned && answer.id === newCommentAnswerId ? (
-              <form
-                onSubmit={(event) => {
-                  event.preventDefault();
-                  const handleAddComment = async () => {
-                    const userId = await fetchUsers().then((json) => {
-                      console.log("user data:", json);
-                      const matchedUsers = json.filter(
-                        (user) => user.name === newCommentAuthor
-                      );
-                      console.log("hi", matchedUsers);
-                      if (matchedUsers.length === 0) {
-                        const newUserId = uuidv4();
-                        addUser({
-                          id: newUserId,
-                          name: newCommentAuthor,
-                        });
-                        return newUserId;
-                      } else {
-                        return matchedUsers[0].id;
-                      }
-                    });
-                    const newUserId = uuidv4();
-                    const newTimestamp = Date.now();
-                    await addComment({
-                      id: newUserId,
-                      title: "",
-                      body: newCommentBody,
-                      userId: userId,
-                      postId: postData.id,
-                      answerId: answer.id,
-                      timestamp: newTimestamp,
-                    });
-                    setNewCommentBody("");
-                    setNewCommentAuthor("");
-                    fetchData();
-                  };
+              <div className="my-3">
+                <Input
+                  id="body"
+                  required={true}
+                  label="new body content: "
+                  type="textarea"
+                  value={editContentBody}
+                  onInputChange={(event) => {
+                    setEditContentBody(event.target.value);
+                  }}
+                />
+                <button type="submit">Make change</button>
+              </div>
+            </form>
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
 
-                  handleAddComment();
-                  setIsCommentFormExpanded(false);
-                  setNewCommentAnswerId("");
+      {answers
+        .sort((answerA, answerB) => {
+          return answerB.timestamp - answerA.timestamp;
+        })
+        .map((answer) => {
+          return (
+            <div key={"answers" + answer.id} className="container">
+              <br />
+              <ContentBody type="Answer: " content={answer} />
+              {isAdmin ? (
+                <>
+                  <button
+                    onClick={() => {
+                      setEditContentId(answer.id);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  {answer.id === editContentId ? (
+                    <form
+                      onSubmit={(event) => {
+                        event.preventDefault();
+                        const handleUpdateAnswer = async () => {
+                          const newTimestamp = Date.now();
+                          await updateAnswer(answer.id, {
+                            id: answer.id,
+                            title: "",
+                            body: editContentBody,
+                            userId: answer.userId,
+                            postId: post.id,
+                            timestamp: newTimestamp,
+                          });
+                          setEditContentBody("");
+                          setEditContentId("");
+                          fetchData();
+                        };
+                        handleUpdateAnswer();
+                      }}
+                    >
+                      <div className="my-3">
+                        <Input
+                          id="body"
+                          required={true}
+                          label="new body content: "
+                          type="textarea"
+                          value={editContentBody}
+                          onInputChange={(event) => {
+                            setEditContentBody(event.target.value);
+                          }}
+                        />
+                        <button type="submit">Make change</button>
+                      </div>
+                    </form>
+                  ) : (
+                    <></>
+                  )}
+                </>
+              ) : (
+                <></>
+              )}
+              {comments.length > 0 ? (
+                <p>Comments sorted from latest to oldest</p>
+              ) : (
+                <></>
+              )}
+              {comments
+                .filter((comment) => {
+                  return comment.answerId === answer.id;
+                })
+                .sort((commentA, commentB) => {
+                  return commentB.timestamp - commentA.timestamp;
+                })
+                .map((comment) => {
+                  return (
+                    <>
+                      <ContentBody
+                        key={"comment" + comment.id}
+                        type="Comment: "
+                        content={comment}
+                      />
+
+                      {isAdmin ? (
+                        <>
+                          <button
+                            onClick={() => {
+                              setEditContentId(comment.id);
+                            }}
+                          >
+                            Edit
+                          </button>
+                          {comment.id === editContentId ? (
+                            <form
+                              onSubmit={(event) => {
+                                event.preventDefault();
+                                const handleUpdateComment = async () => {
+                                  const newTimestamp = Date.now();
+                                  await updateComment(comment.id, {
+                                    id: comment.id,
+                                    title: "",
+                                    body: editContentBody,
+                                    userId: comment.userId,
+                                    postId: comment.postId,
+                                    answerId: comment.answerId,
+                                    timestamp: newTimestamp,
+                                  });
+                                  setEditContentBody("");
+                                  setEditContentId("");
+                                  fetchData();
+                                };
+                                handleUpdateComment();
+                              }}
+                            >
+                              <div className="my-3">
+                                <Input
+                                  id="body"
+                                  required={true}
+                                  label="new body content: "
+                                  type="textarea"
+                                  value={editContentBody}
+                                  onInputChange={(event) => {
+                                    setEditContentBody(event.target.value);
+                                  }}
+                                />
+                                <button type="submit">Make change</button>
+                              </div>
+                            </form>
+                          ) : (
+                            <></>
+                          )}
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                    </>
+                  );
+                })}
+
+              <button
+                onClick={(event) => {
+                  if (!isCommentFormExpaned) {
+                    setIsCommentFormExpanded(true);
+                    setNewCommentAnswerId(answer.id);
+                  } else {
+                    // if already expanded, then one more click will make it collapse
+                    if (answer.id === newCommentAnswerId) {
+                      setIsCommentFormExpanded(false);
+                      setNewCommentAnswerId("");
+                    } else {
+                      setNewCommentAnswerId(answer.id);
+                    }
+                  }
                 }}
               >
-                {/* TODO: make body content have larger box */}
-                <div className="my-3">
-                  <Input
-                    id="body"
-                    required={true}
-                    label="body content: "
-                    type="textarea"
-                    value={newCommentBody}
-                    onInputChange={(event) => {
-                      setNewCommentBody(event.target.value);
-                    }}
-                  />
-                </div>
-                <div className="my-3">
-                  <Input
-                    id="author"
-                    required={true}
-                    label="author name: "
-                    type="text"
-                    value={newCommentAuthor}
-                    onInputChange={(event) => {
-                      setNewCommentAuthor(event.target.value);
-                    }}
-                  />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Make new comment
-                </button>
-              </form>
-            ) : (
-              <></>
-            )}
-            <br />
-          </div>
-        );
-      })}
+                Add a comment to this answer
+              </button>
+              {isCommentFormExpaned && answer.id === newCommentAnswerId ? (
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    const handleAddComment = async () => {
+                      const userId = await fetchUsers().then((json) => {
+                        console.log("user data:", json);
+                        const matchedUsers = json.filter(
+                          (user) => user.name === newCommentAuthor
+                        );
+                        console.log("hi", matchedUsers);
+                        if (matchedUsers.length === 0) {
+                          const newUserId = uuidv4();
+                          addUser({
+                            id: newUserId,
+                            name: newCommentAuthor,
+                          });
+                          return newUserId;
+                        } else {
+                          return matchedUsers[0].id;
+                        }
+                      });
+                      const newUserId = uuidv4();
+                      const newTimestamp = Date.now();
+                      await addComment({
+                        id: newUserId,
+                        title: "",
+                        body: newCommentBody,
+                        userId: userId,
+                        postId: post.id,
+                        answerId: answer.id,
+                        timestamp: newTimestamp,
+                      });
+                      setNewCommentBody("");
+                      setNewCommentAuthor("");
+                      fetchData();
+                    };
+
+                    handleAddComment();
+                    setIsCommentFormExpanded(false);
+                    setNewCommentAnswerId("");
+                  }}
+                >
+                  {/* TODO: make body content have larger box */}
+                  <div className="my-3">
+                    <Input
+                      id="body"
+                      required={true}
+                      label="body content: "
+                      type="textarea"
+                      value={newCommentBody}
+                      onInputChange={(event) => {
+                        setNewCommentBody(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <Input
+                      id="author"
+                      required={true}
+                      label="author name: "
+                      type="text"
+                      value={newCommentAuthor}
+                      onInputChange={(event) => {
+                        setNewCommentAuthor(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Make new comment
+                  </button>
+                </form>
+              ) : (
+                <></>
+              )}
+              <br />
+            </div>
+          );
+        })}
 
       <br />
       <h2>Unsatisified with current answers? Write your own!</h2>
@@ -197,7 +372,7 @@ export default function Post() {
               title: "",
               body: newAnswerBody,
               userId: userId,
-              postId: postData.id,
+              postId: post.id,
               timestamp: newTimestamp,
             });
             setNewAnswerBody("");
